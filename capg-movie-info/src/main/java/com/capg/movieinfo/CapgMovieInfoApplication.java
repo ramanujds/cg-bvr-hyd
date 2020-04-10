5674b7c5290e568dec04aead42ea43e9b45951e7
ramanujdas <ramanujds9@gmail.com>
2020-04-10 10:52:58 +0530

    Movie Catelog Service

---
+++ capg-movie-info/src/main/java/com/capg/movieinfo/CapgMovieInfoApplication.java
@@ -0,0 +1,13 @@
+package com.capg.movieinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapgMovieInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapgMovieInfoApplication.class, args);
+	}
+
+}
---
+++ capg-movie-info/src/test/java/com/capg/springboot/CapgMovieInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capg.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapgMovieInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
