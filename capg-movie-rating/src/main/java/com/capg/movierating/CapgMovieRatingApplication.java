17a3655018670ad051707752c1be99ef4b1b8c81
ramanujdas <ramanujds9@gmail.com>
2020-04-10 10:53:42 +0530

    Movie Rating Service

---
+++ capg-movie-rating/src/main/java/com/capg/movierating/CapgMovieRatingApplication.java
@@ -0,0 +1,13 @@
+package com.capg.movierating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapgMovieRatingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapgMovieRatingApplication.class, args);
+	}
+
+}
