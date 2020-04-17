1edb5947c7b0771c505e3fd511283c27b326574e
ramanujdas <ramanujds9@gmail.com>
2020-04-17 17:18:35 +0530

    Added Jenkins

--- capg-movie-catelog/src/test/java/com/capg/moviecatelog/service/CapgMovieCatelogApplicationTests.java
+++ capg-movie-catelog/src/test/java/com/capg/moviecatelog/service/CapgMovieCatelogApplicationTests.java
@@ -1,13 +1,13 @@
-package com.capg.springboot;
+package com.capg.moviecatelog.service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
+@SpringBootTest
 class CapgMovieCatelogApplicationTests {
 
-//	@Test
-//	void contextLoads() {
-//	}
+	@Test
+	void contextLoads() {
+	}
 
 }
---
+++ capg-movie-catelog/src/test/java/com/capg/moviecatelog/service/MovieCatelogServiceTest.java
@@ -0,0 +1,51 @@
+package com.capg.moviecatelog.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.capg.moviecatelog.model.MovieCatelog;
+import com.capg.moviecatelog.repo.MovieCatelogRepo;
+
+@SpringBootTest
+class MovieCatelogServiceTest {
+
+	@Autowired
+	private MovieCatelogRepo repo;
+	//MovieCatelog catelog=new MovieCatelog(5001L, "KGF");
+	
+	
+
+	@Test
+	void testGetMovieCatelog() {
+		if(repo.existsById(1001L)) {
+		MovieCatelog catelog=repo.getOne(1001L);
+		//System.out.println(catelog);
+		assertNotNull(catelog);
+		}
+		
+	}
+
+//	@Test
+//	void testGetAllMovieCatelog() {
+//		
+//	}
+
+//	@Test
+//	void testAddMovieCatelog() {
+//		MovieCatelog catelog=new MovieCatelog(4001L, "Bahubali");
+//		assertTrue(repo.existsById(catelog.getId()));
+//		
+//	}
+//	
+//	@AfterEach
+//	void cleanUp() throws Exception {
+//		
+//		repo.delete(catelog);
+//	}
+
+}
